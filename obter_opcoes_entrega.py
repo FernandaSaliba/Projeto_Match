69a0fc4e02f7814ea987681ea7a5e7b6abf22c81
Arthur Vasconcelos Piorotti <arthur.piorotti11@gmail.com>
2023-11-14 07:48:27 -0300

    Update obter_opcoes_entrega.py

--- obter_opcoes_entrega.py
+++ obter_opcoes_entrega.py
@@ -1,8 +1,6 @@
 def obter_opcoes_entrega(cep):
     opcoes_entrega = []
     if cep:
-        # Verifique o CEP e adicione as opções de entrega com base no CEP aqui
-        # Neste exemplo, adicionaremos apenas duas opções fictícias.
         opcoes_entrega.append("1. Entrega padrão")
         opcoes_entrega.append("2. Entrega expressa")
     return opcoes_entrega
